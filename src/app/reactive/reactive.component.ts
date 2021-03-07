bd7da80177d458d689575d16e584ee5c897bceec
manik_kashyap <manikkashyap0786@gmail.com>
2021-03-07 17:01:43 +0530

    template form

--- src/app/reactive/reactive.component.ts
+++ src/app/reactive/reactive.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {MustMatch} from 'src/app/helper/helper.validator'
 
 @Component({
   selector: 'app-reactive',
@@ -7,26 +8,46 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./reactive.component.css']
 })
 export class ReactiveComponent implements OnInit {
+  Registerform!: FormGroup;
 
-  constructor() { }
+  constructor( private formbuilder:FormBuilder) { }
+
+  submitted = false
 
   ngOnInit(): void {
-  }
-    name = new FormControl('');
-    Registerform = new FormGroup({
-    name : new FormControl(''),
-    email : new FormControl(''),
-    contact : new FormControl(''),
-    Password : new FormControl(''),
-    confirm: new FormControl(''),
-    Gender : new FormControl(''),
-    Location : new FormControl(''),
-    terms : new FormControl('')
+
+
+    this.Registerform = this.formbuilder.group({
+    name : ['',[Validators.required , Validators.minLength(5)]],
+    email : ['',[Validators.required,Validators.email]],
+    contact : ['',[Validators.required,Validators.minLength(10),Validators.maxLength(10)]],
+    Password : ['',Validators.required],
+    confirm: ['',Validators.required],
+    Gender : ['',Validators.required],
+    Location : ['',Validators.required],
+    terms : [false,Validators.requiredTrue],
+  },
+  {
+    validators : MustMatch('Password','confirm')
   });
+
+  }
 
   getdetais()
   {
     console.log(this.Registerform.value);
+  }
+
+  get f(){
+    return this.Registerform.controls;
+  }
+
+  onsubmit(){
+    this.submitted=true;
+    if(this.Registerform.invalid)
+    {
+      return
+    }
   }
 
 }
