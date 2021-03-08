227d9b30a6cdfbad955e25237812b3b143a5d9f9
manik_kashyap <manikkashyap0786@gmail.com>
2021-03-08 19:01:19 +0530

    Revert "template form"
    This reverts commit bd7da80177d458d689575d16e584ee5c897bceec.

--- src/app/reactive/reactive.component.ts
+++ src/app/reactive/reactive.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import {MustMatch} from 'src/app/helper/helper.validator'
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
@@ -8,46 +7,26 @@ import {MustMatch} from 'src/app/helper/helper.validator'
   styleUrls: ['./reactive.component.css']
 })
 export class ReactiveComponent implements OnInit {
-  Registerform!: FormGroup;
 
-  constructor( private formbuilder:FormBuilder) { }
-
-  submitted = false
+  constructor() { }
 
   ngOnInit(): void {
-
-
-    this.Registerform = this.formbuilder.group({
-    name : ['',[Validators.required , Validators.minLength(5)]],
-    email : ['',[Validators.required,Validators.email]],
-    contact : ['',[Validators.required,Validators.minLength(10),Validators.maxLength(10)]],
-    Password : ['',Validators.required],
-    confirm: ['',Validators.required],
-    Gender : ['',Validators.required],
-    Location : ['',Validators.required],
-    terms : [false,Validators.requiredTrue],
-  },
-  {
-    validators : MustMatch('Password','confirm')
-  });
-
   }
+    name = new FormControl('');
+    Registerform = new FormGroup({
+    name : new FormControl(''),
+    email : new FormControl(''),
+    contact : new FormControl(''),
+    Password : new FormControl(''),
+    confirm: new FormControl(''),
+    Gender : new FormControl(''),
+    Location : new FormControl(''),
+    terms : new FormControl('')
+  });
 
   getdetais()
   {
     console.log(this.Registerform.value);
-  }
-
-  get f(){
-    return this.Registerform.controls;
-  }
-
-  onsubmit(){
-    this.submitted=true;
-    if(this.Registerform.invalid)
-    {
-      return
-    }
   }
 
 }
